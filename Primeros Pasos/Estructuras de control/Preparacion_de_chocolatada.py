e706eab11f47c5a643a457cf8dbf51b0222f0e49
IgnacioAScotto <scottoignacioagustin@gmail.com>
2026-02-06 19:19:42 -0300

    Como hacer una chocolatada termianda

---
+++ Primeros Pasos/Estructuras de control/Preparacion_de_chocolatada.py
@@ -0,0 +1,16 @@
+print('Anda a la cocina')
+print('Abri la heladera')
+
+hay_leche = input('Hay leche? (Y/N): ')
+hay_chocolate = input('Hay Nesquick?: (Y/N): ')
+
+if hay_leche != "Y" or hay_chocolate != "Y":
+    print("Anda al super")
+    if hay_leche != "Y":
+        print('Compra leche')
+    if hay_chocolate != "Y":
+        print('Compa Nesquick')
+
+print("Poner 2 cucharadas de Nesquick en la taza")
+print('Poner 1/4 de taza de leche y revolver')
+print('Llenar la taza de leche')
