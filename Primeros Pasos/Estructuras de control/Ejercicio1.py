a959eb12add3b24f6d80e0df3b11fc201a6a37ef
IgnacioAScotto <scottoignacioagustin@gmail.com>
2026-02-07 02:07:59 -0300

    Correccion de tarea de nesquick y operaciones booleanas

--- Primeros Pasos/Estructuras de control/Ejercicio1.py
+++ Primeros Pasos/Estructuras de control/Ejercicio1.py
@@ -8,3 +8,24 @@ numero_de_jugador = int(input('Ingresa el numero del 1 al 10 que crees que es: '
 
 if numero_de_jugador == numero:
     print('Adivinaste, felicitaciones')
+
+
+
+#Ejercicio pero con numeros random y que no se pueda elegir otro numero
+
+import random
+
+print('BIENVENIDO AL ADIVINADOR')
+numero = random.randint(1, 10)
+numero_de_jugador = int(input('Ingresa el numero del 1 al 10 que crees que es: '))
+
+if numero_de_jugador >10:
+    print('Te pasate, el maximo es 10')
+
+if numero_de_jugador <1:
+    print('Te quedaste corto')
+
+if numero_de_jugador == numero:
+    print('Adivinaste, felicitaciones')
+
+print(f'El numero ganador era {numero}')
