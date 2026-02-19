0907a02c7ba67bc5fe23e7aa9a631d3e309663aa
IgnacioAScotto <scottoignacioagustin@gmail.com>
2026-02-19 20:06:16 -0300

    "Aprendiendo el while" Terminado

--- 2-Datos y Control/1-While.py
+++ 2-Datos y Control/1-While.py
@@ -2,10 +2,18 @@ respuesta = None
 
 while respuesta != "A" and respuesta != "B" and respuesta !="C":
     respuesta = input('Que opciones preferis? [A, B, C]: ')
+    print('Probando')
+
+
+    print('Probando 2.0') #Esto sigue perteneciendo al while por la tabulacion
+    print("Esto sigue siendo parte del bucle while.")
+
 
 if respuesta =="A":
     print('Elegiste bien')
 elif respuesta == "B":
     print('podrias haber elegido mejor')
-else:
+elif respuesta =="B":
     print('Elegiste la peor respuesta')
+else:
+    print('No me diste una respuesta con sentido') #Esto no hace falta con el while, por lo que el else podria estar en el respuesta == "C"
