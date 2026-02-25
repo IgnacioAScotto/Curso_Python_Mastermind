91fe70fd6c72e927625da951a749cf45caa95b85
IgnacioAScotto <scottoignacioagustin@gmail.com>
2026-02-25 09:48:46 -0300

    Ejercicio lista de numeros terminado

--- 2-Datos y Control/4.6-for_ejercicios.py
+++ 2-Datos y Control/4.6-for_ejercicios.py
@@ -5,20 +5,17 @@ Outuput
 numero_mas_chico = 1
 numero_mas+grande = 6 '''
 
-lista_numeros_usuario = []
-agregar_numero = None
+numeros_de_usuario = []
 
-while agregar_numero != "Q":
-    input_de_usuario = (input("Agrega un numero a la lista: ['Q' para salir]: "))
-    if input_de_usuario == "Q":
-        break
-    elif input_de_usuario in lista_numeros_usuario:
-        print(f"El nro {input_de_usuario} ya se encuentra en la lista")
-    else:
-        lista_numeros_usuario.append(input_de_usuario)
-        print(f'Se agrego el numero {input_de_usuario} a la lista de numeros')
+numero_introducido = int(input('Introduce un numero a la lista: '))
+numeros_de_usuario.append(numero_introducido)
 
-print(f'La lista de numeros es: {lista_numeros_usuario}')
+while input("Desea introducir otro numero? [S/N]: ") == "S":
+    numero_introducido = int(input('Introduce un numero a la lista: '))
+    numeros_de_usuario.append(numero_introducido)
 
+print(numeros_de_usuario)
 
+print(f'El numero mas grande es: {max(numeros_de_usuario)}')
+print(f'El numero mas chico es: {min(numeros_de_usuario)}')
 
