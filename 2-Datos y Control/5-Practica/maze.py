0a7a0d00e4102fc1faa93b7fcc80a11f12698d36
IgnacioAScotto <scottoignacioagustin@gmail.com>
2026-02-25 10:59:24 -0300

    Creado el mapa y ubicado el jugador en el maze

---
+++ 2-Datos y Control/5-Practica/maze.py
@@ -0,0 +1,25 @@
+POS_X = 0
+POS_Y = 1
+MAP_WIDTH = 20
+MAP_HEIGHT = 15
+
+my_position = [3, 1]
+
+my_position[POS_X]
+my_position[POS_Y]
+
+print('+' + '-' * MAP_WIDTH * 3 + '+')
+
+for coordinate_y in range(MAP_HEIGHT):
+    print('|', end ='')
+    for coordinate_x in range(MAP_WIDTH):
+        if coordinate_x == my_position[POS_X] and coordinate_y == my_position[POS_Y]:
+            print(" @ ", end='')
+        else:
+            print('   ', end='')
+    print('|')
+
+print('+' + '-' * MAP_WIDTH * 3 + '+')
+
+
+
