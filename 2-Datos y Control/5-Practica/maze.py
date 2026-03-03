bcf4f2d7762923f859ece08f36391301d03209b2
IgnacioAScotto <scottoignacioagustin@gmail.com>
2026-03-03 10:49:16 -0300

    Mapa terminado, moviemiento de jugador teminado, aparecer en el otro borde terminado sin corregir

--- 2-Datos y Control/5-Practica/maze.py
+++ 2-Datos y Control/5-Practica/maze.py
@@ -1,3 +1,6 @@
+import os
+import readchar
+
 POS_X = 0
 POS_Y = 1
 MAP_WIDTH = 20
@@ -8,18 +11,48 @@ my_position = [3, 1]
 my_position[POS_X]
 my_position[POS_Y]
 
-print('+' + '-' * MAP_WIDTH * 3 + '+')
 
-for coordinate_y in range(MAP_HEIGHT):
-    print('|', end ='')
-    for coordinate_x in range(MAP_WIDTH):
-        if coordinate_x == my_position[POS_X] and coordinate_y == my_position[POS_Y]:
-            print(" @ ", end='')
-        else:
-            print('   ', end='')
-    print('|')
 
-print('+' + '-' * MAP_WIDTH * 3 + '+')
+while True: 
+    #Draw Map
+    print('+' + '-' * MAP_WIDTH * 3 + '+')
+    for coordinate_y in range(MAP_HEIGHT):
+        print('|', end ='')
+        for coordinate_x in range(MAP_WIDTH):
+            if coordinate_x == my_position[POS_X] and coordinate_y == my_position[POS_Y]:
+                print(" @ ", end='')
+            else:
+                print('   ', end='')
+        print('|')
+
+    print('+' + '-' * MAP_WIDTH * 3 + '+')
+
+
+    #Ask user ehere he wants to move
+
+    #direction = input('Donde te queres mover? [WASD]: ')
+    direction = readchar.readchar()
+
+    if direction == 'w':
+        my_position[POS_Y] -=1
+    elif direction == 's':
+        my_position[POS_Y] +=1
+    elif direction == 'a':
+        my_position[POS_X] -=1
+    elif direction == 'd':
+        my_position[POS_X] +=1
+
+    os.system('cls')
+
+    #Ejercicio, aparecer en el otro borde del mapa
+    if my_position[POS_X] == -1:
+        my_position[POS_X] = 19
+    elif my_position[POS_X] == 20:
+        my_position[POS_X] = 0
+    elif my_position[POS_Y] == -1:
+        my_position[POS_Y] = 14
+    elif my_position[POS_Y] == 15:
+        my_position[POS_Y] = 0
 
 
 
